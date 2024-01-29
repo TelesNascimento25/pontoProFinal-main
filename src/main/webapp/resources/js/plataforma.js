a04cb91b621a88768b4edeb5dddaae1dd8640711
Darlysson Teles <123102867+TelesNascimento25@users.noreply.github.com>
2024-01-29 19:44:46 -0300

    Update plataforma.js

--- src/main/webapp/resources/js/plataforma.js
+++ src/main/webapp/resources/js/plataforma.js
@@ -1,5 +1,30 @@
 var horariosTrabalho = [];
 
+function calcular(tipo) {
+    var entrada = document.getElementById("entradaHorario").value;
+    var saida = document.getElementById("saidaHorario").value;
+    var entradaMarcacao = document.getElementById("entradaMarcacao").value;
+    var saidaMarcacao = document.getElementById("saidaMarcacao").value;
+
+    $.post("/pontoPro/calculo", { entrada: entrada, saida: saida, entradaMarcacao: entradaMarcacao, 
+        saidaMarcacao: saidaMarcacao }, function(data) {
+        var tabela = document.getElementById("tabela" + tipo);
+        var corpoTabela = tabela.getElementsByTagName('tbody')[0];
+        corpoTabela.innerHTML = "";
+
+        data[tipo].forEach(function(item) {
+            var novaLinha = corpoTabela.insertRow(-1);
+            var celulaEntrada = novaLinha.insertCell(0);
+            var celulaSaida = novaLinha.insertCell(1);
+            var celulaTipo = novaLinha.insertCell(2);
+
+            celulaEntrada.innerHTML = item.entrada;
+            celulaSaida.innerHTML = item.saida;
+            celulaTipo.innerHTML = item[tipo.toLowerCase()];
+        });
+    });
+};
+
 function adicionarRegistro(formId, tabelaId) {
     var form = document.getElementById(formId);
     var entrada = form.querySelector('#entradaHorario').value;
@@ -22,19 +47,42 @@ function adicionarRegistro(formId, tabelaId) {
     var tabela = document.getElementById(tabelaId);
     var thead = tabela.getElementsByTagName('thead')[0];
 
-    var novaLinhaThead = thead.insertRow(0);
-    var celulaEntrada = novaLinhaThead.insertCell(0);
-    var celulaSaida = novaLinhaThead.insertCell(1);
-    celulaEntrada.innerHTML = 'Horário de Entrada no Trabalho: ' + entrada;
-    celulaSaida.innerHTML = 'Horário de Saída no Trabalho: ' + saida;
+        var novaLinhaThead = thead.insertRow(0);
+        var celulaEntrada = novaLinhaThead.insertCell(0);
+        var celulaSaida = novaLinhaThead.insertCell(1);
+        celulaEntrada.innerHTML = 'Horário de Entrada no Trabalho: ' + entrada;
+        celulaSaida.innerHTML = 'Horário de Saída no Trabalho: ' + saida;
 
     horariosTrabalho.push({ entrada: entrada, saida: saida, marcacoes: [] });
 }
 
+
 function adicionarMarcacao(formId, tabelaId) {
+	debugger;
     var form = document.getElementById(formId);
     var entradaMarcacao = form.querySelector('#entradaMarcacao').value;
     var saidaMarcacao = form.querySelector('#saidaMarcacao').value;
+
+    var erro = document.getElementById('erro');
+    erro.style.display = 'none'; 
+
+    if (!entradaMarcacao || !saidaMarcacao) {
+        erro.innerHTML = 'Por favor, preencha os horários de entrada e saída.';
+        erro.style.display = 'block'; 
+        return;
+    }
+
+    if (saidaMarcacao <= entradaMarcacao) {
+        erro.innerHTML = 'O horário de saída não pode ser menor ou igual ao horário de entrada.';
+        erro.style.display = 'block'; 
+        return;
+    }
+
+    if (horariosTrabalho.length == 0) {
+        erro.innerHTML = 'Por favor, adicione um horário de trabalho antes de adicionar uma marcação.';
+        erro.style.display = 'block'; 
+        return;
+    }
 
     var tabela = document.getElementById(tabelaId);
     var tbody = tabela.getElementsByTagName('tbody')[0];
@@ -48,9 +96,65 @@ function adicionarMarcacao(formId, tabelaId) {
     horariosTrabalho[horariosTrabalho.length - 1].marcacoes.push({ entrada: entradaMarcacao, saida: saidaMarcacao });
 }
 
+
+function calcularDiferenca() {
+    var atrasos = [];
+    var horasExtras = [];
+
+    horariosTrabalho.forEach(function (horario) {
+        var entradaTrabalho = new Date("1970-01-01 " + horario.entrada);
+        var saidaTrabalho = new Date("1970-01-01 " + horario.saida);
+
+        horario.marcacoes.forEach(function (marcacao) {
+            var entradaMarcacao = new Date("1970-01-01 " + marcacao.entrada);
+            var saidaMarcacao = new Date("1970-01-01 " + marcacao.saida);
+
+            if (entradaMarcacao > entradaTrabalho) {
+                atrasos.push({ entrada: horario.entrada, saida: marcacao.entrada, atraso: calcularDiferencaMinutos(entradaTrabalho, entradaMarcacao) });
+            }
+            if (saidaMarcacao < saidaTrabalho) {
+                atrasos.push({ entrada: marcacao.saida, saida: horario.saida, atraso: calcularDiferencaMinutos(saidaMarcacao, saidaTrabalho) });
+            }
+
+            if (entradaMarcacao < entradaTrabalho) {
+                horasExtras.push({ entrada: entradaMarcacao, saida: entradaTrabalho, horaExtra: calcularDiferencaMinutos(entradaMarcacao, entradaTrabalho) });
+            }
+            if (saidaMarcacao > saidaTrabalho) {
+                horasExtras.push({ entrada: saidaTrabalho, saida: saidaMarcacao, horaExtra: calcularDiferencaMinutos(saidaTrabalho, saidaMarcacao) });
+            }
+        });
+    });
+
+    document.getElementById('mensagemErroHoraExtra').style.display = 'none';
+    document.getElementById('mensagemSucessoAtraso').style.display = 'none';
+
+    if (atrasos.length === 0) {
+        document.getElementById('mensagemSucessoAtraso').style.display = 'block';
+    }
+
+    if (horasExtras.length === 0) {
+        document.getElementById('mensagemErroHoraExtra').style.display = 'block';
+    }
+
+    return {
+        atrasos: atrasos,
+        horasExtras: horasExtras
+    };
+}
+
+
 function calcularDiferencaMinutos(inicio, fim) {
     var diff = (fim - inicio) / 60000; 
     return Math.round(diff);
+}
+
+function calcularAtrasos() {
+    var diferenca = calcularDiferenca();
+    var atrasos = diferenca.atrasos;
+
+    limparTabela("tabelaAtraso");
+
+    preencherTabelaAtraso("tabelaAtraso", atrasos);
 }
 
 function calcularHorasExtras() {
@@ -62,63 +166,70 @@ function calcularHorasExtras() {
     preencherTabelaHoraExtra("tabelaHoraExtra", horasExtras);
 }
 
-function calcularDiferenca() {
-    var atrasos = [];
-    var horasExtras = [];
-
-    horariosTrabalho.forEach(function (bloco) {
-        var entradaTrabalho = new Date("1970-01-01 " + bloco.entrada);
-        var saidaTrabalho = new Date("1970-01-01 " + bloco.saida);
-
-        bloco.marcacoes.forEach(function (marcacao) {
-            var entradaMarcacao = new Date("1970-01-01 " + marcacao.entrada);
-            var saidaMarcacao = new Date("1970-01-01 " + marcacao.saida);
-
-            if (entradaMarcacao < entradaTrabalho) {
-                horasExtras.push({ entrada: marcacao.entrada, saida: bloco.entrada, horaExtra: calcularDiferencaMinutos(entradaMarcacao, entradaTrabalho) });
-            }
-
-            if (saidaMarcacao > saidaTrabalho) {
-                horasExtras.push({ entrada: bloco.saida, saida: marcacao.saida, horaExtra: calcularDiferencaMinutos(saidaTrabalho, saidaMarcacao) });
-            }
-
-            if (entradaMarcacao > entradaTrabalho) {
-                atrasos.push({ entrada: bloco.entrada, saida: marcacao.entrada, atraso: calcularDiferencaMinutos(entradaTrabalho, entradaMarcacao) });
-            }
-
-            if (saidaMarcacao < saidaTrabalho) {
-                atrasos.push({ entrada: marcacao.saida, saida: bloco.saida, atraso: calcularDiferencaMinutos(saidaMarcacao, saidaTrabalho) });
-            }
-        });
-    });
-
-    document.getElementById('mensagemErroHoraExtra').style.display = horasExtras.length === 0 ? 'block' : 'none';
-    document.getElementById('mensagemSucessoAtraso').style.display = atrasos.length === 0 ? 'block' : 'none';
-
-    return {
-        atrasos: atrasos,
-        horasExtras: horasExtras
-    };
+function limparTabela(idTabela) {
+    var tabela = document.getElementById(idTabela);
+    var corpoTabela = tabela.getElementsByTagName('tbody')[0];
+    corpoTabela.innerHTML = "";
 }
 
-function limparTabela(tabelaId) {
-    var tabela = document.getElementById(tabelaId);
-    var tbody = tabela.getElementsByTagName('tbody')[0];
-    tbody.innerHTML = '';
-}
+function preencherTabelaHoraExtra(idTabela, dados) {
+    var tabela = document.getElementById(idTabela);
+    var corpoTabela = tabela.getElementsByTagName('tbody')[0];
 
-function preencherTabelaHoraExtra(tabelaId, horasExtras) {
-    var tabela = document.getElementById(tabelaId);
-    var tbody = tabela.getElementsByTagName('tbody')[0];
+    dados.forEach(function (item) {
+        var novaLinha = corpoTabela.insertRow(-1);
+        var celulaEntradaPrevista = novaLinha.insertCell(0);
+        var celulaBatimentoPonto = novaLinha.insertCell(1);
+        var celulaHoraExtra = novaLinha.insertCell(2);
 
-    horasExtras.forEach(function (horaExtra) {
-        var novaLinha = tbody.insertRow(-1);
-        var celulaPrevisto = novaLinha.insertCell(0);
-        var celulaBatido = novaLinha.insertCell(1);
-        var celulaTempo = novaLinha.insertCell(2);
+        celulaEntradaPrevista.innerHTML = formatarHora(item.entrada); 
+        celulaBatimentoPonto.innerHTML = formatarHora(item.saida); 
+        celulaHoraExtra.innerHTML = item.horaExtra;
 
-        celulaPrevisto.innerHTML = horaExtra.entrada;
-        celulaBatido.innerHTML = horaExtra.saida;
-        celulaTempo.innerHTML = horaExtra.horaExtra;
+        celulaHoraExtra.classList.add('text-success');
     });
 }
+
+
+function formatarHora(data) {
+    var opcoes = { hour: '2-digit', minute: '2-digit' };
+    return data.toLocaleTimeString('pt-BR', opcoes);
+}
+
+
+function millisecondsToTime(ms) {
+    var seconds = Math.floor((ms / 1000) % 60);
+    var minutes = Math.floor((ms / (1000 * 60)) % 60);
+    var hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    return hours + ":" + minutes;
+}
+
+
+function preencherTabelaAtraso(idTabela, dados) {
+    var tabela = document.getElementById(idTabela);
+    var corpoTabela = tabela.getElementsByTagName('tbody')[0];
+
+    dados.forEach(function (item) {
+        var novaLinha = corpoTabela.insertRow(-1);
+        var celulaEntrada = novaLinha.insertCell(0);
+        var celulaSaida = novaLinha.insertCell(1);
+        var celulaAtraso = novaLinha.insertCell(2);
+
+        celulaEntrada.innerHTML = item.entrada;
+        celulaSaida.innerHTML = item.saida;
+        celulaAtraso.innerHTML = item.atraso;
+
+        celulaAtraso.classList.add('text-danger');
+    });
+}
+
+$(document).ready(function () {
+    $('#navTabs a').on('click', function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+});
